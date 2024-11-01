5d43f575356d69f074a340bb13f2a575a8acc512
Asa Bejraputra <gamegame.gtboyz@gmail.com>
2024-11-01 14:04:09 +0100

    add scraping features

--- main.py
+++ main.py
@@ -6,4 +6,28 @@ from bs4 import BeautifulSoup
 # assign the alias onto the file, like we do with variable name.
 with open('home.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
+    ###print(content)      # the content should be printed out onto the terminal
+
+    # create an instance of beautifulsoup, then specify the method
+    soup = BeautifulSoup(content, 'lxml')
+    ###print(soup.prettify())  # the shuld be printed out onto terminal
+
+    # grab some specific informations. in this case, we want to grab all <h5> tags
+    
+    ###tags = soup.find('h5')  # this line only finds out first h5 element, then stops execution since we have 3 'h5' tags in the page.
+    ###courses_html_tags = soup.find_all('h5')      # this will return all h5 tags in page 
+    # iterate to display what we've got
+    ###for course in courses_html_tags:
+    ###    print(course.text)
+
+    # build up some program to gather the course and price, respectively
+    # gather all 'div' contents which contain class='card'. since python reserve 'class' keyword, we need to add underscore after 'class'
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards: 
+
+        # scope what we want in gathering. in this case is text in course name, and text in button which contains strings of price.
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]    # split the whole strings with whitespace, then gather the last element, where's the price located.
+
+        # print out an output
+        print(f'{course_name} costs {course_price}')
